aa5f34897cf1a38a20428d2c3bae271f532e6473
sunwoo1098 <sunwoo1098@naver.com>
2022-05-19 09:37:43 +0900

    07장까지 완성된 소스코드

--- src/Movie.js
+++ src/Movie.js
@@ -4,21 +4,17 @@ import './Movie.css';
 
 function Movie({title, year, summary, poster, genres}) {
     return (
-        <div className="movie">
+        <div className='movie'>
             <img src={poster} alt={title} title={title}/>
-            <div className="movie__data">
-                <h3 className="movie__title">{title}</h3>
-                <h5 className="movie__year">{year}</h5>
-                <ul className="movie__genres">
+            <div className='movie__data'>
+                <h3 className='movie__title'>{title}</h3>
+                <h5 className='movie__year'>{year}</h5>
+                <ul className='movie__genres'>
                     {genres.map((genre, index) => {
-                        return (
-                            <li key={index} className="movie__genre">
-                                {genre}
-                            </li>
-                        );
+                        return <li key={index} className='movie__genre'>{genre}</li>
                     })}
                 </ul>
-                <p className="movie_summary">{summary.slice(0, 180)}...</p>
+                <p className='movie_summary'>{summary.slice(0, 180)}...</p>
             </div>
         </div>
     );
